2e700bde02d539577e9949d839ddfe3dd8d49740
root <root@localhost.localdomain>
2016-01-12 10:37:03 +0300

    user input and calc

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * Created by root on 1/12/16.
+ */
+public class Calculator {
+
+    private int result;
+
+
+    public void add(int param, int param2)
+    {
+
+        result=result + param + param2;
+
+    }
+
+    public int get()
+    {
+        return result;
+    }
+
+
+}
--- calculate.java
+++ calculate.java
@@ -1,8 +1,41 @@
+package com.company;
+
+import java.lang.*;
+import java.util.Scanner;
+
 public class calculate
 {
-	public static void main(String[] args)
+	public static void main(String[] arg)
 
 	{
-		System.out.println("Calculate");
+		Calculator calc = new Calculator();
+		Scanner scan = new Scanner(System.in);
+		String exit = "no";
+		try
+		{
+			while(!exit.equals("yes"))
+			{
+				System.out.println("Enter first arg : ");
+				String first=scan.next();
+				System.out.println("Enter sec arg : ");
+				String sec =scan.next();
+				int fir=Integer.valueOf(first);
+				int se=Integer.valueOf(sec);
+
+				calc.add(fir,se);
+
+
+				System.out.println(calc.get());
+
+				System.out.println("Exit yes/no");
+				exit=scan.next();
+
+			}
+
+		}
+		finally {
+			scan.close();
+		}
+
 	}
 }
